eb9cb5dc36bd3fb737ed84f36683581f8f13f6ac
Marcinccc <czarnecki.marcin12@o2.pl>
2023-06-12 18:36:50 +0200

    add product page with initial state

---
+++ src/pages/Product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Product.css';
+
+function Product(){
+    return(
+        <>
+   <div className="container product">
+  <div className="row">
+    <div className="col">Column  <img className="imgproduct" src="https://scontent.fpoz3-1.fna.fbcdn.net/v/t39.30808-6/298266585_568987074871676_5123833989163155586_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a26aad&_nc_ohc=fhKpn2qxkxgAX8phr86&_nc_ht=scontent.fpoz3-1.fna&oh=00_AfD7nY45nKJiwD-1yGS8TmXi5_IOag8UFYLcjxAt1WtLXA&oe=648CCD78"></img></div>
+    <div className="col"> </div>
+  </div>
+</div>
+    
+      
+        </>
+    )
+}
+
+
+export default Product ;
